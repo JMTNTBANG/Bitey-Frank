b5a0aac4ade9dcca4597a701445ad37855e804d1
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2023-02-03 20:09:55 -0600

    Minor Fixes
    * Blank Response when using /poll total  on a poll with no voters
    * Moved Code to add each result to the embed to a separate function to fix duplication
    * Moved /poll total code to a separate function to support calling from the code

--- commands/poll.py
+++ commands/poll.py
@@ -1,91 +1,14 @@
 import bot
 import discord
+from threading import Timer
+import time
 
 
 def import_command():
 
-    # Create Command Group
-    pollCommands = discord.app_commands.Group(
-        name='poll',
-        description='Make a Poll'
-    )
-
-    # Add Command Group to Tree
-    bot.tree.add_command(pollCommands)
-
-    # Main Command
-    @pollCommands.command(
-        name="-",
-        description="Make a Poll"
-    )
-    async def self(Interaction:discord.Interaction,
-        question:str,
-        option1:str,
-        option2:str,
-        option3:str='',
-        option4:str='',
-        option5:str='',
-        option6:str='',
-        option7:str='',
-        option8:str='',
-        option9:str='',
-        option10:str=''
-    ):
-        await Interaction.response.send_message('Creating Poll',ephemeral=True)
-        embed=discord.Embed(
-                    title=f'Poll: {question}',
-                    description='',
-                    color=discord.Color.green()
-                    ).set_author(
-                        name=Interaction.user.name,
-                        icon_url=Interaction.user.avatar
-                    )
-        embed.add_field(name='1️⃣', value=option1, inline=False)
-        embed.add_field(name='2️⃣', value=option2, inline=False)
-        if option3 != '':
-            embed.add_field(name='3️⃣', value=option3, inline=False)
-        if option4 != '':
-            embed.add_field(name='4️⃣', value=option4, inline=False)
-        if option5 != '':
-            embed.add_field(name='5️⃣', value=option5, inline=False)
-        if option6 != '':
-            embed.add_field(name='6️⃣', value=option6, inline=False)
-        if option7 != '':
-            embed.add_field(name='7️⃣', value=option7, inline=False)
-        if option8 != '':
-            embed.add_field(name='8️⃣', value=option8, inline=False)
-        if option9 != '':
-            embed.add_field(name='9️⃣', value=option9, inline=False)
-        if option10 != '':
-            embed.add_field(name='🔟', value=option10, inline=False)
-        if isinstance(Interaction.channel, discord.TextChannel):
-            poll= await Interaction.channel.send(
-                embed=embed)
-            await poll.add_reaction('1️⃣')
-            await poll.add_reaction('2️⃣')
-            if option3 != '':
-                await poll.add_reaction('3️⃣')
-            if option4 != '':
-                await poll.add_reaction('4️⃣')
-            if option5 != '':
-                await poll.add_reaction('5️⃣')
-            if option6 != '':
-                await poll.add_reaction('6️⃣')
-            if option7 != '':
-                await poll.add_reaction('7️⃣')
-            if option8 != '':
-                await poll.add_reaction('8️⃣')
-            if option9 != '':
-                await poll.add_reaction('9️⃣')
-            if option10 != '':
-                await poll.add_reaction('🔟')
-
-    @pollCommands.command(
-        name='total',
-        description='Get a tally of the most recent Untimed Poll'
-    )
-    async def self2(Interaction:discord.Interaction, messageid:str=''):
-        await Interaction.response.send_message('Calculating...')
+    async def total(Interaction:discord.Interaction, messageid:str='', donottouch:bool=False):
+        if donottouch == False:
+            await Interaction.response.send_message('Calculating...')
         global poll
         poll = discord.Message
         if isinstance(Interaction.channel, discord.TextChannel):
@@ -207,90 +130,133 @@ def import_command():
                     icon_url=Interaction.user.avatar
                 )
 
+            async def generateField(reactors, number):
+                if len(oneReactors) != 0:
+                    embed.add_field(
+                        name=number,
+                        value=f'{field["value"]} -- **{round(len(reactors)/totalReactors*100)}%** ***{len(reactors)} User(s)***',
+                        inline=False
+                        )
+                else:
+                    embed.add_field(
+                        name=number,
+                        value=f'{field["value"]} -- **0%** ***0 User(s)***',
+                        inline=False
+                        )
+
             for field in pollEmbed.to_dict().get('fields'):
                 if field['name'] == '1️⃣':
-                    if len(oneReactors) != 0:
-                        embed.add_field(
-                            name='1️⃣',
-                            value=f'{field["value"]} -- **{round(len(oneReactors)/totalReactors*100)}%** ***{len(oneReactors)} User(s)***',
-                            inline=False
-                            )
+                    await generateField(oneReactors,'1️⃣')
                 elif field['name'] == '2️⃣':
-                    if len(twoReactors) != 0:
-                        embed.add_field(
-                            name='2️⃣',
-                            value=f'{field["value"]} -- **{round(len(twoReactors)/totalReactors*100)}%** ***{len(twoReactors)} User(s))***',
-                            inline=False
-                            )
+                    await generateField(twoReactors,'2️⃣')
                 elif field['name'] == '3️⃣':
-                    if len(threeReactors) != 0:
-                        embed.add_field(
-                            name='3️⃣',
-                            value=f'{field["value"]} -- **{round(len(threeReactors)/totalReactors*100)}%** ***{len(threeReactors)} User(s))***',
-                            inline=False
-                            )
+                    await generateField(threeReactors,'3️⃣')
                 elif field['name'] == '4️⃣':
-                    if len(fourReactors) != 0:
-                        embed.add_field(
-                            name='4️⃣',
-                            value=f'{field["value"]} -- **{round(len(fourReactors)/totalReactors*100)}%** ***{len(fourReactors)} User(s)***',
-                            inline=False
-                            )
+                    await generateField(fourReactors,'4️⃣')
                 elif field['name'] == '5️⃣':
-                    if len(fiveReactors) != 0:
-                        embed.add_field(
-                            name='5️⃣',
-                            value=f'{field["value"]} -- **{round(len(fiveReactors)/totalReactors*100)}%** ***{len(fiveReactors)} User(s))***',
-                            inline=False
-                            )
+                    await generateField(fiveReactors,'5️⃣')
                 elif field['name'] == '6️⃣':
-                    if len(sixReactors) != 0:
-                        embed.add_field(
-                            name='6️⃣',
-                            value=f'{field["value"]} -- **{round(len(sixReactors)/totalReactors*100)}%** ***{len(sixReactors)} User(s))***',
-                            inline=False
-                            )
+                    await generateField(sixReactors,'6️⃣')
                 elif field['name'] == '7️⃣':
-                    if len(sevenReactors) != 0:
-                        embed.add_field(
-                            name='7️⃣',
-                            value=f'{field["value"]} -- **{round(len(sevenReactors)/totalReactors*100)}%** ***{len(sevenReactors)} User(s))***',
-                            inline=False
-                            )
+                    await generateField(sevenReactors,'7️⃣')
                 elif field['name'] == '8️⃣':
-                    if len(eightReactors) != 0:
-                        embed.add_field(
-                            name='8️⃣',
-                            value=f'{field["value"]} -- **{round(len(eightReactors)/totalReactors*100)}%** ***{len(eightReactors)} User(s))***',
-                            inline=False
-                            )
+                    await generateField(eightReactors,'8️⃣')
                 elif field['name'] == '9️⃣':
-                    if len(nineReactors) != 0:
-                        embed.add_field(
-                            name='9️⃣',
-                            value=f'{field["value"]} -- **{round(len(nineReactors)/totalReactors*100)}%** ***{len(nineReactors)} User(s))***',
-                            inline=False
-                            )
+                    await generateField(nineReactors,'9️⃣')
                 elif field['name'] == '🔟':
-                    if len(tenReactors) != 0:
-                        embed.add_field(
-                            name='🔟',
-                            value=f'{field["value"]} -- **{round(len(tenReactors)/totalReactors*100)}%** ***{len(tenReactors)} User(s))***',
-                            inline=False
-                            )
-                
-            await Interaction.channel.send(embed=embed)
+                    await generateField(tenReactors,'🔟')
+    
+            await poll.channel.send(embed=embed)
 
-            # embed.add_field(
-            #     name='1️⃣',
-            #     value=f'{str(pollEmbed.to_dict["1️⃣"])} {round(len(oneReactors)/totalReactors*100)}% ({len(oneReactors)-1} User(s))')
+    # Create Command Group
+    pollCommands = discord.app_commands.Group(
+        name='poll',
+        description='Make a Poll'
+    )
 
-            # await Interaction.channel.send(
-            #     embed=discord.Embed(
-            #         title='Tallied Votes',
-            #         description=msg,
-            #         color=discord.Color.green()
-            #         ).set_author(
-            #             name=Interaction.user.name,
-            #             icon_url=Interaction.user.avatar
-            #         ))
+    # Add Command Group to Tree
+    bot.tree.add_command(pollCommands)
+
+    # Main Command
+    @pollCommands.command(
+        name="-",
+        description="Make a Poll"
+    )
+    async def self(Interaction:discord.Interaction,
+        question:str,
+        option1:str,
+        option2:str,
+        # timer:float=0.0,
+        option3:str='',
+        option4:str='',
+        option5:str='',
+        option6:str='',
+        option7:str='',
+        option8:str='',
+        option9:str='',
+        option10:str=''
+    ):
+        await Interaction.response.send_message('Creating Poll',ephemeral=True)
+        embed=discord.Embed(
+                    title=f'Poll: {question}',
+                    description='',
+                    color=discord.Color.green()
+                    ).set_author(
+                        name=Interaction.user.name,
+                        icon_url=Interaction.user.avatar
+                    )
+        embed.add_field(name='1️⃣', value=option1, inline=False)
+        embed.add_field(name='2️⃣', value=option2, inline=False)
+        if option3 != '':
+            embed.add_field(name='3️⃣', value=option3, inline=False)
+        if option4 != '':
+            embed.add_field(name='4️⃣', value=option4, inline=False)
+        if option5 != '':
+            embed.add_field(name='5️⃣', value=option5, inline=False)
+        if option6 != '':
+            embed.add_field(name='6️⃣', value=option6, inline=False)
+        if option7 != '':
+            embed.add_field(name='7️⃣', value=option7, inline=False)
+        if option8 != '':
+            embed.add_field(name='8️⃣', value=option8, inline=False)
+        if option9 != '':
+            embed.add_field(name='9️⃣', value=option9, inline=False)
+        if option10 != '':
+            embed.add_field(name='🔟', value=option10, inline=False)
+        if isinstance(Interaction.channel, discord.TextChannel):
+        #     if timer != 0:
+        #         embed.description = f'Poll Ends <t:{round(time.time()+int(timer*60))}:R>'
+            poll= await Interaction.channel.send(
+                embed=embed)
+            # if timer != 0:
+            #     def timerend(Interaction:discord.Interaction, messageid, donottouch):
+            #         async def timerend2(Interaction:discord.Interaction, messageid, donottouch):
+            #             await total(Interaction, messageid, donottouch)
+            #         await timerend2(Interaction,messageid,donottouch)
+            #     pollTimer=Timer(timer, total, (Interaction,poll.id,True))
+            #     await pollTimer.start()
+            await poll.add_reaction('1️⃣')
+            await poll.add_reaction('2️⃣')
+            if option3 != '':
+                await poll.add_reaction('3️⃣')
+            if option4 != '':
+                await poll.add_reaction('4️⃣')
+            if option5 != '':
+                await poll.add_reaction('5️⃣')
+            if option6 != '':
+                await poll.add_reaction('6️⃣')
+            if option7 != '':
+                await poll.add_reaction('7️⃣')
+            if option8 != '':
+                await poll.add_reaction('8️⃣')
+            if option9 != '':
+                await poll.add_reaction('9️⃣')
+            if option10 != '':
+                await poll.add_reaction('🔟')
+
+    @pollCommands.command(
+        name='total',
+        description='Get a tally of the most recent Untimed Poll'
+    )
+    async def self2(Interaction:discord.Interaction, messageid:str='', donottouch:bool=False):
+        await total(Interaction,messageid,donottouch)
