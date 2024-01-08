eeb81418963f4feb4594c5ac6b92bb4056a7467a
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2024-01-08 13:16:45 -0600

    Fixed /channelstat bug

--- commands/channelstat.py
+++ commands/channelstat.py
@@ -10,17 +10,17 @@ def import_command():
     )
     # Code to Run Here
     async def self(interaction: discord.Interaction):
-        response = "Calculating"
-        await interaction.response.send_message(response)
+        await interaction.response.send_message("Calculating... (This may take awhile depending on how many messages there are)")
         channels = []
+        total_message_count = 0
         for channel in interaction.guild.text_channels:
             message_count = 0
             async for message in channel.history(limit=None):
                 message_count += 1
+                total_message_count += 1
             channels.append({"mention": channel.mention, "count": message_count})
-            response += "."
-            await interaction.edit_original_response(content=response)
         message = ""
+        message += f"## Total Server Messages: {total_message_count}\n"
 
         def key(e):
             return e['count']
@@ -28,4 +28,5 @@ def import_command():
         for channel in channels:
             message += f"{channel['mention']}: {channel['count']} Messages\n"
         embed = discord.Embed(title="Channel Message Statistics", description=message)
-        await interaction.edit_original_response(content="Completed!", embed=embed)
+        org_msg = await interaction.original_response()
+        await org_msg.reply(content="Completed!", embed=embed)
