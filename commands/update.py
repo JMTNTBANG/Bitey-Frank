4a44dc021e638c311ae8403e192b3b814f326323
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2024-01-08 13:18:38 -0600

    changed /reboot to /update

--- commands/update.py
+++ commands/update.py
@@ -6,11 +6,11 @@ import sys
 def import_command():
     # Command Info
     @bot.tree.command(
-        name="reboot",
-        description="Reboot the Bot"
+        name="update",
+        description="Update the Bot to latest commits"
     )
     # Code to Run Here
     async def kill(interaction: discord.Interaction):
-        await interaction.response.send_message('Rebooting...')
+        await interaction.response.send_message('Updating...')
         sys.exit()
     
