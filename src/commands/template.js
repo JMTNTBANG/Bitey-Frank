14d26f3fa6fee4c293254ff30680e6066a870059
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2024-01-11 21:54:12 -0600

    Purge Command Functionality

--- src/commands/template.js
+++
@@ -1,6 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-
-module.exports = {
-  data: new SlashCommandBuilder().setName("Name").setDescription("Description"),
-  async execute(ctx) {},
-};
--- src/index.js
+++ src/index.js
@@ -10,28 +10,6 @@ const frank = new Client({
   ],
 });
 
-frank.commands = new Collection();
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-      frank.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
-}
-
 const frank_emojis = [
   "<:chonkfronk:1040418775129927752>",
   "<:frank3:1040422477433675857>",
@@ -42,34 +20,6 @@ const frank_emojis = [
 
 frank.on(Events.ClientReady, (ctx) => {
   console.log(`Logged in as ${ctx.user.tag}`);
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  }
 });
 
 frank.on(Events.MessageCreate, (ctx) => {
