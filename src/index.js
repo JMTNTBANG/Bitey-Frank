5c73feb05ab1f05a794796a13dde7eb6dcb6bea0
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2024-01-11 22:52:12 -0600

    Added App Command Interpreter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const fs = require("node:fs");
+const path = require("node:path");
 const { token } = require("../config.json");
 const { Client, Collection, Events, IntentsBitField } = require("discord.js");
 
@@ -18,8 +20,59 @@ const frank_emojis = [
   "<:jpeg:1042214971460829326>",
 ];
 
+// Command Loader
+frank.commands = new Collection();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      frank.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+// Client Events
 frank.on(Events.ClientReady, (ctx) => {
   console.log(`Logged in as ${ctx.user.tag}`);
+});
+
+frank.on(Events.InteractionCreate, async (ctx) => {
+  const command = ctx.client.commands.get(ctx.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${ctx.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(ctx);
+  } catch (error) {
+    console.error(error);
+    if (ctx.replied || ctx.deferred) {
+      await ctx.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await ctx.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
 });
 
 frank.on(Events.MessageCreate, (ctx) => {
@@ -35,4 +88,4 @@ frank.on(Events.MessageCreate, (ctx) => {
   }
 });
 
-frank.login(process.env.DEBUGTOKEN);
+frank.login(token);
