9d45bbc101ff07fd63d09293924244065d551e5f
James Miller <32822936+JMTNTBANG@users.noreply.github.com>
2024-01-14 03:24:18 -0600

    Added /update

---
+++ src/commands/tools/update.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("update")
+    .setDescription("Update the Bot to latest commits"),
+  async execute(ctx) {
+    ctx.reply("Updating...")
+    throw new Error("Admin Requested Bot Update")
+  },
+};
